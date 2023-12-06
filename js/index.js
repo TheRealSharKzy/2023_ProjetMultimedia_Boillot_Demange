3ab34b3901660e384087a6a8d4178ec3e058f038
Hugo Demange <hugo.demange8@etu.univ-lorraine.fr>
2023-12-07 00:16:53 +0100

    merge domain code and structure (finishing touch)

--- js/index.js
+++ js/index.js
@@ -1,127 +1,147 @@
+"use strict"
 
-//Il y a surement un moyen d'enlever ces variables mais j'ai pas trouvé
-let currAnimalRecognitionGame;
-let animalList = [];
-let nbTimesClicked = 0;
+const GAMES = {
+    "training": LearnGame,
+    "recognition": RecognitionGame
+}
 
 window.addEventListener("load", async () => {
+    const selector = document.getElementById("language_select");
+    const navBar = document.getElementsByTagName("nav")[0];
+    const grid = document.getElementById("grid")
+
     // source of data
-    let raw_data = await fetch('json/animalsTranslation.json').then((response) => response.json());
-    let datas = raw_data;
-    writeLanguagesList(Object.keys(raw_data))
-    let language = "english"
+    let data = await fetch('json/animalsTranslation.json').then((response) => response.json());
 
+    let currentGame;
 
-    /**
-     * @type {HTMLSelectElement}
-     */
-    let selector = document.getElementById("language_select");
-
-    window.addEventListener("input", () => {
-        // Je suis pas sûr que la voix change (à revoir)
+    // deal with language changing
+    let language;
+    writeLanguagesList(selector, Object.keys(data))
+    let listener = () => {
         language = selector.value
-        let translations = datas[language]
-        constructAnimalList(translations, language)
-    })
 
-    // Game 1 = training
-    let game1 = document.getElementById("game1")
-    game1.addEventListener("click",() => changeGame("training", datas, language));
+        constructAnimalList()
+        if (typeof currentGame === "object")
+            currentGame = new currentGame.constructor(data[language])
+    };
+    window.addEventListener("input", listener)
+    listener()
 
-    // Game 2 = Recognition game
-    let game2 = document.getElementById("game2")
-    game2.addEventListener("click",() => changeGame("recognition", datas, language));
+    // create game selector
+    let gamesList = Object.values(GAMES)
+    navBar.innerHTML = ""
+    for (let i = 0; i < gamesList.length; i++) {
+        let GameConstructor = gamesList[i]
+
+        let htmlElement = document.createElement("span")
+        htmlElement.innerText = Object.keys(GAMES)[i]
+        let startGame = () => {
+
+            // highlight default game (the first)
+            for (let child of navBar.children) {
+                child.classList.remove("selected")
+            }
+            htmlElement.classList.add("selected")
+
+            currentGame = new GameConstructor(data[language])
+        };
+        htmlElement.addEventListener("click", startGame)
+        navBar.appendChild(htmlElement)
+
+        if (i === 0) startGame()
+    }
+
+    function constructAnimalList(){
+        let translations = data[language]
+        grid.innerHTML = ""
+
+        for (let i = 0; i < translations.animals.length; i++) {
+            const current = translations.animals[i]
+
+            let imgElement = document.createElement("img");
+            imgElement.src = current.path
+            imgElement.addEventListener("click", function (event) {currentGame.onclick(current, event.currentTarget)})
+            translations.animals[i].htmlElement = imgElement
+            grid.appendChild(imgElement)
+        }
+    }
 })
 
 /**
- *
+ * @param selector {HTMLSelectElement}
  * @param languages {string[]}
  */
-function writeLanguagesList(languages) {
-    /**
-     * @type {HTMLSelectElement}
-     */
-    let selector = document.getElementById("language_select");
+function writeLanguagesList(selector, languages) {
     selector.innerHTML = ""
     for (let lang of languages) {
         let optionElement = document.createElement("option");
         optionElement.text = lang
         selector.appendChild(optionElement)
     }
-
 }
 
-function speechAnimalName(animal, language = "english"){
-    const msg = new SpeechSynthesisUtterance(animal);
-    msg.lang = language;
+function speech(sentence, languageCode= "en"){
+    if (typeof sentence !== "string")
+        if (typeof sentence.translation === "string")
+            sentence = sentence.translation
+
+    const msg = new SpeechSynthesisUtterance(sentence);
+    msg.lang = languageCode;
     msg.rate = 0.8;
     window.speechSynthesis.speak(msg);
 }
 
-function speech(words){
-    const msg = new SpeechSynthesisUtterance(words);
-    msg.lang = "english";
-    msg.rate = 0.8;
-    window.speechSynthesis.speak(msg);
+function LearnGame(translations) {
+    this.languageCode = translations.languageCode
 }
 
-function constructAnimalList(jsonData, language, game = "training"){
-    let grid = document.getElementById("grid")
-    let i = 1
-    grid.innerHTML = ""
-    animalList = [];
-
-    for (const current of jsonData.animals) {
-        animalList.push(current.name);
-
-        let imgElement = document.createElement("img");
-        imgElement.src = current.path
-        imgElement.id = "img" + i++;
-        if (game === "training" ) imgElement.addEventListener("click", () => speechAnimalName(current.translation, current.languageCode))
-        else imgElement.addEventListener("click", () => recognitionGameClick(current.name, language, jsonData))
-        grid.appendChild(imgElement)
-    }
+LearnGame.prototype.speak = function (text) {
+    speech(text, this.languageCode)
 }
 
-// Donne la traduction de l'animal dans la langue courante
-function getAnimalTranslation(animal, languages){
-    for (let i = 0; i<languages.animals.length;i++){
-        let current = languages.animals[i];
-        if (current.name === animal){
-            return current.translation;
-        }
-    }
+LearnGame.prototype.onclick = function (animalClicked, htmlTarget) {
+    this.speak(animalClicked)
 }
 
-// Lance le jeu de reconnaissance
-function recognitionGameStart(jsonData){
-    // Animal aléatoirement pris dans la liste
-    currAnimalRecognitionGame = animalList[Math.round(Math.random()*animalList.length - 1)];
-    // Traduction de l'animal choisi aléatoirement avec la langue choisi
-    let translation = getAnimalTranslation(currAnimalRecognitionGame, jsonData)
-    speechAnimalName(translation);
+function RecognitionGame(translations) {
+    this.animals = translations.animals
+    this.languageCode = translations.languageCode
+    this.refresh()
 }
-
-// Quand on est dans le jeu de reconnaissance et que l'on clique sur une image
-function recognitionGameClick(animalClicked,language, jsonData){
-    if (animalClicked === currAnimalRecognitionGame){
-        nbTimesClicked = 0;
+RecognitionGame.prototype.speak = function (text) {
+    speech(text, this.languageCode)
+}
+RecognitionGame.prototype.refresh = function () {
+    this.currentAnimal = this.animals[Math.floor(Math.random() * this.animals.length)]
+    this.nbTry = 0;
+    this.speak(this.currentAnimal)
+}
+RecognitionGame.prototype.onclick = function (animalClicked, htmlTarget){
+    if (animalClicked === this.currentAnimal){
         speech("You win");
         //Quand on gagne on relance une partie
         // Peut-être mettre un délai avant de relancer la partie
-        recognitionGameStart(jsonData);
+        this.refresh()
     } else {
         //Mettre les cases en sur lesquels on a cliqué mais ne sont pas bonnes en rouge
-        nbTimesClicked++
+        this.nbTry++
         // Je laisse 3 essais
-        if (nbTimesClicked<3) {
+        if (this.nbTry<3) {
             speech("You lose but you can retry");
-            speechAnimalName(currAnimalRecognitionGame);
+            this.speak(this.currentAnimal)
         } else {
+            let htmlElementAnimal = this.currentAnimal.htmlElement
             speech("You lost completely. The good answer was")
+            htmlElementAnimal.classList.add("highlight")
+            let flash = setInterval(() => htmlElementAnimal.classList.toggle("highlight"), 500);
+            setTimeout(() => {
+                clearInterval(flash)
+                htmlElementAnimal.classList.remove("highlight")
+            }, 5000)
             // Mettre en vert la bonne case
             // Attendre quelques secondes
-            recognitionGameStart(jsonData)
+            this.refresh()
         }
     }
 }
@@ -130,15 +150,6 @@ function recognitionGameClick(animalClicked,language, jsonData){
 function changeGame(game, jsonData, language){
     constructAnimalList(jsonData[language],language, game);
     if (game === "recognition") recognitionGameStart(jsonData[language])
-}
-
-/**
- *
- * @param event {Event}
- */
-function refreshLanguage(event) {
-    let target = event.target;
-    console.log(target.value)
 }
 
 
